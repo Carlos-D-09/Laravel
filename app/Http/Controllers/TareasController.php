a00a38c1f4129a43477721b6831c147922f28777
Carlos-D-09 <carlos.medina1311@alumnos.udg.mx>
2022-03-07 14:31:44 -0600

    creacion de un controlador de tipo resources

--- app/Http/Controllers/TareasController.php
+++ app/Http/Controllers/TareasController.php
@@ -3,23 +3,82 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 
 class TareasController extends Controller
 {
-    public function index(){
-        $tareas = DB::table('tareas')->get();
-        // dd($tareas);
-        //la función compact permite pasar variables a la pagina hacia la cual estamos siendo reedirigidos
-        return view('tareas.indexTareas',compact('tareas'));
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
     }
 
-    public function create(){
-        return view('tareas.formTareas');
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
     }
-    public function store(){
-        //validacion y limpieza
-        //Guarda a DB
-        //Redireccionar
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -28,10 +28,9 @@ Route::get('/hola-mundo',function(){
     return view('paginas/hola-mundo');
 });
 
+//Rutas para la manipulacion de tareas
 Route::get('/tareas', [TareasController::class, 'index']);
-
 Route::get('tarea/create', [TareasController::class, 'create']);
-
 Route::post('tarea/store', [TareasController::class, 'store']);
 
 Route::get('/grabaciones/{nombre}/{year?}/{cantidad?}',function($nombre, $year = null, $cantidad = null){
