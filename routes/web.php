43c1f785d2a49747729f46dd404998a4559393b4
Carlos Daniel Medina Sahagun <carlos.medina1311@alumnos.udg.mx>
2022-03-27 20:56:55 -0600

    Instalación de breeze

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,6 @@
 <?php
 
-use App\Http\Controllers\TareasController;
 use Illuminate\Support\Facades\Route;
-// use Illuminate\Support\Facades\DB; solo necesario si no uso un modelo
 
 /*
 |--------------------------------------------------------------------------
@@ -19,33 +17,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-//Crea ruta para retornar un msg
-Route::get('/hola-mundo',function(){
-    return 'Hola Mundo';
-});
-// Crear una ruta para retornar una vista
-Route::get('/hola-mundo',function(){
-    return view('paginas/hola-mundo');
-});
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
-//Rutas por default para la manipulacion de tareas con un controlador resources
-Route::resource('/tarea',TareasController::class);
-// Route::get('/tarea', [TareasController::class, 'index']);
-// Route::get('tarea/create', [TareasController::class, 'create']);
-// Route::post('tarea/store', [TareasController::class, 'store']);
-// show
-// edit
-// update
-// delete
-
-Route::get('/grabaciones/{nombre}/{year?}/{cantidad?}',function($nombre, $year = null, $cantidad = null){
-    return view('paginas.grabaciones', compact('nombre', 'year', 'cantidad'));
-});
-
-Route::get('/bienvenida', function(){
-    return view('bienvenida');
-});
-
-Route::get('/contacto', function(){
-    return view('contacto');
-});
+require __DIR__.'/auth.php';
