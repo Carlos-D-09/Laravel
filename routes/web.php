8019ea455b8879bf9295246c9dd3557867160dfa
Carlos Daniel Medina Sahagun <carlos.medina1311@alumnos.udg.mx>
2022-03-27 20:59:42 -0600

    Recuperacion de las rutas

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,36 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+//Crea ruta para retornar un msg
+Route::get('/hola-mundo',function(){
+    return 'Hola Mundo';
+});
+// Crear una ruta para retornar una vista
+Route::get('/hola-mundo',function(){
+    return view('paginas/hola-mundo');
+});
+
+//Rutas por default para la manipulacion de tareas con un controlador resources
+Route::resource('/tarea',TareasController::class);
+// Route::get('/tarea', [TareasController::class, 'index']);
+// Route::get('tarea/create', [TareasController::class, 'create']);
+// Route::post('tarea/store', [TareasController::class, 'store']);
+// show
+// edit
+// update
+// delete
+
+Route::get('/grabaciones/{nombre}/{year?}/{cantidad?}',function($nombre, $year = null, $cantidad = null){
+    return view('paginas.grabaciones', compact('nombre', 'year', 'cantidad'));
+});
+
+Route::get('/bienvenida', function(){
+    return view('bienvenida');
+});
+
+Route::get('/contacto', function(){
+    return view('contacto');
+});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
