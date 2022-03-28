c90bba01723ffc96915117e9104e945fe78a6a6b
Carlos Daniel Medina Sahagun <carlos.medina1311@alumnos.udg.mx>
2022-03-27 21:33:43 -0600

    Arreglo del login y register de breeze (modificar modelo de user para agregarle lo que falta)

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,10 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Contracts\Auth\Authenticatable as AuthAuthenticatable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class User extends Model
+class User extends Model implements AuthAuthenticatable
 {
     use HasFactory;
+    use Authenticatable;
+    protected $fillable = ['name', 'email', 'password'];
+    public $timestamps = false;
 }
